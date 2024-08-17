ee4235ff084dadede2c04320b71080e9c4b10098
dya <96499165+Dhiya2007@users.noreply.github.com>
2024-08-17 04:07:59 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,3 +38,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 element.textContent = displayText;
             });
         });
+        const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
